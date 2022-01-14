4192a22eacb7d4127bfbe3f0ea41ba8e88b92641
Sharri Light <slight@vo-i3.msk.21-school.ru>
2022-01-14 19:27:32 +0300

    15.01.22

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,47 +6,58 @@
 /*   By: slight <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:44:48 by slight            #+#    #+#             */
-/*   Updated: 2022/01/13 00:06:05 by slight           ###   ########.fr       */
+/*   Updated: 2022/01/14 19:23:26 by slight           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "ft_printf.h"
+
+int	addret(int num, int *sum)
+{
+	if (num < 0)
+		return (0);
+	*sum += num;
+	return (1);
+}
+
+void	parse(va_list data, char format, int *sum)
+{
+	if (format == 'c')
+		*sum += print_char((char)va_arg(data, int));
+	else if (format == 'i' || format == 'd')
+		*sum += print_number(va_arg(data, int), 1);
+	else if (format == 's')
+		*sum += print_string((char *)va_arg(data, char *));
+	else if (format == 'u')
+		*sum += print_unsigned_number((t_ui)va_arg(data, int));
+	else if (ft_strchr("xX", format))
+		*sum += print_hex(va_arg(data, t_ui), format);
+	else if (format == 'p')
+		*sum += print_pointer(va_arg(data, t_ull), 16, "0123456789abcdef");
+	else if (format == '%')
+		*sum += print_char('%');
+}
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	data;
+	int		sum;
 
+	sum = 0;
 	va_start(data, format);
-	while(*format)
+	while (*format)
 	{
 		if (*format == '%')
 		{
 			++format;
 			if (ft_strchr(CONVERSIONS, *format))
-				parse(data, *format);
-			else 
-				return (0);
+				parse(data, *format, &sum);
+			else
+				return (-1);
 		}
-		else
-			write(1, format, 1);
+		else if (!addret(write(1, format, 1), &sum))
+			return (-1);
 		++format;
 	}
-	va_end(data, format);
-	return (0);
-}
-
-int main(void)
-{
-	char	c1;
-	char	*c2;
-	int		i;
-	unsigned int	hex;
-	unsigned int	hex1;
-
-	c1 = 'z';
-	c2 = "Andrey";
-	i = -12;
-	hex = 0xff;
-	hex1 = 255;
-	ft_printf("hello %c, My name is %s. Im %d years old. OMG \n%d \n%X \n%%", c1, c2, i, hex, hex1);
+	va_end(data);
+	return (sum);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slight <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 00:27:35 by slight            #+#    #+#             */
+/*   Updated: 2022/01/14 19:01:36 by slight           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -8,32 +20,25 @@
 
 # define CONVERSIONS "cspdiuxX%"
 
-typedef unsigned int t_llong;
-
-typedef struct	s_value{
-	int				i;
-	unsigned int	hex;
-	char			c;
-	char			*s;
-
-}				t_value;
+typedef unsigned int		t_ui;
+typedef unsigned long long	t_ull;
 
 int		print_char(char c);
 int		print_string(char *s);
-int		print_number(int num);
-int		print_hex(long num, char c);
-int		ft_putnbr_base(t_llong num, int osn, char *base);
+int		print_number(int num, int fd);
+int		print_hex(t_ui num, char c);
+int		ft_putnbr_base(t_ui num, int osn, char *base);
 size_t	ft_strlen(const char *s);
-int		ft_putnbr_fd(int n, int fd);
 char	*ft_strchr(const char *s, int c);
-int		parse(va_list data, char format);
-int		count1_power(int n);
-int		jrs1_power(int osn, int st);
-int		count2_power(unsigned int n);
-int		jrs2_power(int osn, unsigned int st);
-int		ft_putunsnbr(unsigned int num);
-int		print_unsigned_number(unsigned int num);
-int		print_pointer(unsigned long num, int osn, char *base);
+void	parse(va_list data, char format, int *sum);
+int		digit_count_of_int(int n);
+t_ull	jrs_power(int osn, int st);
+int		digit_count_of_ui(t_ui n);
+int		jrs2_power(int osn, t_ui st);
+int		print_unsigned_number(t_ui num);
+int		print_pointer(t_ull num, int osn, char *base);
 int		ft_printf(const char *format, ...);
-int		addret(int num, int* sum);
+int		addret(int num, int *sum);
+int		s_len(int n, t_ull m);
+
 #endif
